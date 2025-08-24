964b0878a302d6a4cfd2310b42caf4a8e08274d0
maisonsilene <maisonsilene@gmail.com>
2025-08-24 18:13:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 const SUPABASE_URL = "https://eiewigilwczsciraepnb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpZXdpZ2lsd2N6c2NpcmFlcG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4MDcyMDcsImV4cCI6MjA3MTM4MzIwN30.EZkI_3PyG_8hVdCVUlijnWNE1QmOAuoGskb7e6pkmNU";
 
-// ⚠️ Ne mets jamais ici la "service_role". L’anon est faite pour le front.
-//    Ta sécurité repose sur les RLS (policies) que nous avons configurées.
+// ⚠️ Ne mets JAMAIS la service_role ici (trop puissante).
+//    La sécurité côté navigateur repose sur tes policies RLS.
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
